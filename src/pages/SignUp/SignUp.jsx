ac85b5fc192e6745f4c0a8f243bb2b210f0e5de3
fraciah <karfraciah@gmail.com>
2023-10-18 09:55:16 +0300

    Renamed login and register to sign in and sign up

---
+++ src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,10 @@
+import SignUpForm  from '../../components/form/SignUpForm';
+
+export default function Register() {
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <SignUpForm />
+    </div>
+  );
+}
