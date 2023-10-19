c7fa7a00a449d992d1ffbdc474ff8fb88392969a
fraciah <karfraciah@gmail.com>
2023-10-19 11:59:05 +0300

    Edited styles in inputs

--- src/components/ui/InputField.jsx
+++ src/components/ui/InputField.jsx
@@ -3,9 +3,9 @@ const InputField = (props) => {
     console.log(otherProps);
 
     return (
-        <div className="mb-4">
+        <div className="mb-1 flex flex-col items-start">
             <label className="text-gray-700 text-sm font-bold mb-2">{label}</label>
-            <input className="border rounded  py-2 px-3 text-gray-700" {...otherProps} />
+            <input className="border rounded  py-2 px-3 text-gray-700 w-full" {...otherProps} />
         </div>
     );
 };
--- src/components/ui/PasswordField.jsx
+++ src/components/ui/PasswordField.jsx
@@ -2,9 +2,9 @@ const PasswordField = (props ) => {
     const {label, ...otherProps} = props;
     console.log(otherProps)
     return (
-        <div className="mb-4">
+        <div className="mb-1 flex flex-col items-start">
             <label className="text-gray-700 text-sm font-bold mb-2">{label}</label>
-            <input className="border rounded  py-2 px-3 text-gray-700" {...otherProps} />
+            <input className="border rounded  py-2 px-3 text-gray-700 w-full" {...otherProps} />
         </div>
     );
 };
