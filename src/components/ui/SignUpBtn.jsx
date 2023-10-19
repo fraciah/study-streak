f94487e2908df55bc5bb9a9439baa3de5c90c3da
fraciah <karfraciah@gmail.com>
2023-10-19 11:57:33 +0300

    Changed padding in sign up button

--- src/components/ui/SignUpBtn.jsx
+++ src/components/ui/SignUpBtn.jsx
@@ -1,6 +1,6 @@
 const SignUpBtn = ({ children }) => {
     return (
-        <button className="bg-rose-500 hover:bg-rose-600 text-white font-semibold rounded-3xl px-6 py-3">
+        <button className="bg-rose-500 hover:bg-rose-600 text-white font-semibold rounded-3xl px-7 py-2 mt-1">
             {children}
         </button>
     );
