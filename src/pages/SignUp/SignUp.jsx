6d8cc245ba248c5ce428e29144aee7a6363975bf
fraciah <karfraciah@gmail.com>
2023-10-19 12:02:42 +0300

    Edited styles in the sign up page

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -2,18 +2,18 @@ import SignUpForm  from '../../components/form/SignUpForm';
 
 export default function Register() {
   return (
-    <div className="h-screen bg-slate-300">
+    <div className="h-screen flex justify-center items-center">
 
-      <div className="flex border-2 border-rose-500 rounded-lg">
-        <div className="bg-rose-500 flex items-center ju w-1/3">
+      <div className="flex w-2/3">
+        <div className="bg-rose-500 border-2 border-rose-500 rounded-l-lg flex justify-center items-center w-1/3">
           <h2 className="text-white text-xl">Already have an account?</h2>
         </div>
 
-        <div className="p-5 w-2/3 bg-white">
-          <h1 className="text-rose-500 font-bold text-3xl">Sign Up</h1>
+        <div className="p-5 w-2/3 border-2 border-rose-500 rounded-r-lg bg-white">
+          <h1 className="text-rose-500 font-bold text-3xl text-center">Sign Up</h1>
           <SignUpForm />
         </div>
-        </div>
+      </div>
 
     </div>
   );
