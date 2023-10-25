ca150fd90bc44929c34024e1ed4af6fa2f5b963c
fraciah <karfraciah@gmail.com>
2023-10-25 16:03:41 +0300

    Edited styles in Login and SignUp pages

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -3,15 +3,15 @@ import SignUpBtn from '../../components/ui/SignUpBtn';
 
 export default function LogIn({ goToSignUp }) {
   return (
-    <div className="h-screen flex justify-center items-center">
-
+    <div className="h-screen flex flex-col justify-center items-center bg-logSign bg-no-repeat bg-cover bg-center">
+      <h2 className="text-4xl font-bold mb-5 text-gray-800">Study Streak</h2>
       <div className="flex w-2/3">
-        <div className="bg-rose-500 border-2 border-rose-500 rounded-l-lg flex flex-col justify-center items-center w-1/3">
+        <div className="bg-rose-500 bg-opacity-80 rounded-l-lg flex flex-col justify-center items-center w-1/3">
           <h2 className="text-white text-xl mb-1">Don't have an account?</h2>
           <SignUpBtn className="border border-white" goToSignUp={goToSignUp}/>  {/*className,goToSignUp as props*/}
         </div>
 
-        <div className="p-5 w-2/3 border-2 border-rose-500 rounded-r-lg bg-white">
+        <div className="p-5 w-2/3 rounded-r-lg bg-white bg-opacity-40">
           <h1 className="text-rose-500 font-bold text-3xl text-center">Log In</h1>
           <LogInForm />
         </div>
