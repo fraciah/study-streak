721f133dd3be835e118299326f840fd08d8a0531
fraciah <karfraciah@gmail.com>
2023-11-14 22:41:08 +0300

    Add click functionality to dashboard items

--- src/components/ui/DashBItems.jsx
+++ src/components/ui/DashBItems.jsx
@@ -1,12 +1,18 @@
-const DashBItems = () => {
+const DashBItems = ({ setActivePane }) => {
     return (
         <div className="w-1/4 bg-rose-500">
             <div className="text-white p-4 space-y-3">
                 <h1>Welcome user</h1>
                 <ul className="space-y-3">
-                    <li className="cursor-pointer text-lg font-semibold">Subjects</li>
-                    <li className="cursor-pointer text-lg font-semibold">Calendar</li>
-                    <li className="cursor-pointer text-lg font-semibold">Analysis</li>
+                    <li className="cursor-pointer text-lg font-semibold"
+                        onClick={() => setActivePane('Subjects')}>Subjects
+                    </li>
+                    <li className="cursor-pointer text-lg font-semibold"
+                        onClick={() => setActivePane('Calendar')}>Calendar
+                    </li>
+                    <li className="cursor-pointer text-lg font-semibold"
+                        onClick={() => setActivePane('Analysis')}>Analysis
+                    </li>
                 </ul>
             </div>
         </div>
