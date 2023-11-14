82222f3609bb6edd03d15468f58e30b7b8f142b3
fraciah <karfraciah@gmail.com>
2023-11-14 22:40:41 +0300

    Add AnalysisPane and CalendarPane components.

---
+++ src/components/ui/AnalysisPane.jsx
@@ -0,0 +1,10 @@
+const AnalysisPane = () => {
+    return (
+        <div className="w-3/4 bg-gray-200">
+            <div>
+                <button>Analysis </button>
+            </div>
+        </div>
+    );
+}
+export default AnalysisPane;
---
+++ src/components/ui/CalendarPane.jsx
@@ -0,0 +1,10 @@
+const CalendarPane = () => {
+    return (
+        <div className="w-3/4 bg-gray-200">
+            <div>
+                <button>calendar</button>
+            </div>
+        </div>
+    );
+}
+export default CalendarPane;
