a4cdd72707f9b64033231311c68494a2f73499ba
fraciah <karfraciah@gmail.com>
2023-12-15 18:51:34 +0300

    Refactor Home component layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,16 @@
-import { Link } from 'react-router-dom';
-import homeImg from '../assets/homeImg.jpg';
-
 const Home = () => {
     return (
-        <div className="relative h-screen">
-
-            <img src={homeImg} alt="home" className="absolute top-0 left-0 w-full h-full object-fill" />
-
-            <div className="absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center text-center">
-                <h1 className="text-4xl md:text-5xl lg:text-6xl text-white font-extrabold">Study Streak</h1>
-                <p className="text-sm md:text-lg lg:text-xl text-white font-medium mt-2">Your personal assistant for tracking and improving daily study habits</p>
-                <button className="text-sm md:text-lg lg:text-xl text-white bg-rose-500 hover:bg-rose-600 py-2 px-4 rounded-3xl mt-4 font-medium">
-                    <Link to="/signup">Get started</Link>
-                </button>
+        <div className='bg-homeImg bg-cover bg-center bg-no-repeat h-screen flex flex-col justify-center items-center px-2'>
+            <div className='flex flex-col justify-center items-center space-y-14'>
+                <h1 className='text-cyan-300 text-5xl sm:text-6xl md:text-7xl text-center font-bold'>Study Streak</h1>
+                <div>
+                    <p className='text-stone-300 text-base md:text-lg  text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore quia reiciendis unde optio voluptatibus nihil reprehenderit facilis harum fugiat</p> 
+                    <p className='text-stone-300 text-base md:text-lg text-center'>perspiciatis error voluptatum tempora distinctio cumque, facere quas amet quod dicta.</p>
+                </div>
+            </div>
+            <div className='flex justify-center items- space-x-8 mt-12'>
+                <button className='text-stone-300 border border-cyan-300 py-1 sm:py-2 px-4 sm:px-5 text-base font-medium hover:text-gray-800 hover:bg-cyan-300 transition duration-300'>Sign In</button>
+                <button className='text-stone-300 border border-cyan-300 py-1 sm:py-2 px-4 sm:px-5 text-base font-medium hover:text-gray-800 hover:bg-cyan-300 transition duration-300'>Get started</button>
             </div>
         </div>
     );
