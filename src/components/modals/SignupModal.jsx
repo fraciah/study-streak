056021b7e277d087de5bbf5e9c058c3993a8e7c0
fraciah <karfraciah@gmail.com>
2023-12-16 18:17:14 +0300

    Refactor modal styling

--- src/components/modals/SignupModal.jsx
+++ src/components/modals/SignupModal.jsx
@@ -1,11 +1,11 @@
 const SignupModal = ({setShowSignupModal}) => {
     return(
         <div className="bg-gray-100 bg-opacity-50 flex flex-col justify-center items-center top-0 left-0 w-full h-full fixed">
-            <div className="bg-neutral-700 p-8 h-4/6 w-full sm:w-3/4 md:w-3/5 relative">
-                <p className="text-stone-300 text-2xl absolute top-2 right-2 cursor-pointer"
+            <div className="flex flex-col justify-center items-center bg-neutral-700 p-8 h-4/6 w-full sm:w-3/4 md:w-3/5 relative">
+                <p className="text-stone-300 text-2xl absolute top-4 right-4 cursor-pointer"
                     onClick={() => setShowSignupModal(false)}>&times;</p>
                 <h1 className='text-stone-300 my-8 font-bold text-2xl sm:text-3xl md:text-4xl text-center'>Join Study Streak</h1>
-                <div className="flex justify-center my-12">
+                <div className="flex justify-center my-4">
                     <h3 className="text-stone-300 border border-cyan-500 py-2 px-4 rounded-full hover:text-gray-800 hover:bg-cyan-300 transition duration-300 cursor-pointer font-semibold">Sign up with Google</h3>
                 </div>
                 <div className="my-8">
