662befc47c6cf594e64f1abaaa9d3b51c9c02265
fraciah <karfraciah@gmail.com>
2024-01-08 17:11:04 +0300

    Delete unused components

--- src/components/ui/LogInBtn.jsx
+++
@@ -1,9 +0,0 @@
-const LogInBtn = ({ className, goToLogIn }) => {
-    return(
-        <button className={`bg-rose-500 hover:bg-rose-600 text-white sm:text-base text-sm font-semibold rounded-3xl sm:px-8 px-3 sm:py-2 py-1 my-1 ${className}`} onClick={goToLogIn}>
-            LOG IN
-        </button>
-    )
-}
-
-export default LogInBtn;
--- src/components/ui/SignUpBtn.jsx
+++
@@ -1,9 +0,0 @@
-const SignUpBtn = ({ className, goToSignUp }) => {
-    return (
-        <button className={`bg-rose-500 hover:bg-rose-600 text-white sm:text-base text-sm font-semibold rounded-3xl sm:px-8 px-3 sm:py-2 py-1 my-1 ${className}`} onClick={goToSignUp}>
-            SIGN UP
-        </button>
-    );
-};
-
-export default SignUpBtn;
